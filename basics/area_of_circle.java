dbffa269071e7a927bf28329f3310c7897c94249
Dhyanipatel24 <dhyanipatel379@gmail.com>
2026-04-22 19:31:08 +0530

    Create area_of_circle.java

---
+++ basics/area_of_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AOCircle {
+    public static void main(String args[]){
+        //Area of circle
+        Scanner sc = new Scanner(System.in);
+        float r=sc.nextFloat();
+        float pi= 3.14f;
+        float AOC=pi*r*r;
+        System.out.println(AOC);
+    }
+    
+}
